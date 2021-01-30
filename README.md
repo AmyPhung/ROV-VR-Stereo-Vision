b247a95ec35ec9022cb6186db934a8333c10cdc0
Amy <amyngph@gmail.com>
2021-01-30 07:39:40 -0800

    Added timing for debugging

--- README.md
+++ README.md
@@ -3,4 +3,27 @@ This repository contains a Python app that works with either two live camera fee
 
 
 ## Dependencies
-+ OpenCV `pip install opencv-python`
++ OpenCV: `pip install opencv-python`
++ [pyvirtualcam](https://pypi.org/project/pyvirtualcam/): `pip install pyvirtualcam`
+        + Download the "OBS-VirtualCamX.X.X.zip" option from [this site](https://github.com/CatxFish/obs-virtual-cam/releases)
+        + Open an administrative command prompt
+        + Navigate to the directory
+        + Run these lines in the window
+        ```
+regsvr32 /n /i:1 "obs-virtualcam\bin\32bit\obs-virtualsource.dll"
+regsvr32 /n /i:1 "obs-virtualcam\bin\64bit\obs-virtualsource.dll"
+        ```
+
+Notes:
++ there's two virtual cameras - OBS Virtual Camera and OBS-Camera. OBS Virtual Camera comes from the new OBS update and works with zoom but not Unity, and OBS-Camera comes from the registry edit and works with Unity but not zoom. Go figure
+
+
+
+# Shenanigans
+C:\Program Files\obs-studio\obs-plugins\32bit
+
+
++ Note: OBS must be installed and the registry must be edited - follow the instructions listed [here](https://pypi.org/project/pyvirtualcam/)
+    + Install OBS Studio ([reference](https://obsproject.com/welcome))
+        + Select "I will only be using the virtual camera" during the install process
+    + Install OBS Virtual Cam ([reference](https://github.com/Fenrirthviti/obs-virtual-cam/releases))
--- main.py
+++ main.py
@@ -1,25 +1,88 @@
 import cv2
 import numpy as np
+import pyvirtualcam
+import time
 
 l_vid = cv2.VideoCapture("test_footage/left-footage.MOV")
 r_vid = cv2.VideoCapture("test_footage/right-footage.MOV")
 
-while(True):
-    # Capture frame-by-frame
-    l_ret, l_frame = l_vid.read()
-    r_ret, r_frame = r_vid.read()
+l_framespersecond= int(l_vid.get(cv2.CAP_PROP_FPS))
+r_framespersecond= int(r_vid.get(cv2.CAP_PROP_FPS))
 
-    # # Our operations on the frame come here
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+print("Left FPS: ", l_framespersecond)
+print("Right FPS: ", r_framespersecond)
 
-    stacked = np.vstack([l_frame, r_frame])
-    print("Resolution: ", stacked.shape)
+i = 0
 
-    # Display the resulting frame
-    cv2.namedWindow("over-under frame", cv2.WINDOW_NORMAL)
-    cv2.imshow("over-under frame", stacked)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+with pyvirtualcam.Camera(width=3840, height=4320, fps=100) as virtual_cam:
+    while(True):
+        t1 = time.time()
+
+        # Capture frame-by-frame
+        l_ret, l_frame = l_vid.read()
+        r_ret, r_frame = r_vid.read()
+
+        t2 = time.time()
+        print("T1: ", t2-t1)
+
+        # # Our operations on the frame come here
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        stacked_frame = np.vstack([l_frame, r_frame])
+        t3 = time.time()
+        print("T2: ", t3-t2)
+
+        # print("Resolution: ", stacked_frame.shape)
+        # print(i)
+        i += 1
+
+        t4 = time.time()
+        print("T3: ", t4-t3)
+
+        # Send resulting frame to virtual camera
+        frame = np.zeros((virtual_cam.height, virtual_cam.width, 4), np.uint8) # RGBA
+        t5 = time.time()
+        print("T4: ", t5-t4)
+
+        frame[:,:,:3] = stacked_frame#virtual_cam.frames_sent % 255 # grayscale animation
+        # frame[:,:,3] = 255
+        t6 = time.time()
+        print("T5: ", t6-t5)
+
+        virtual_cam.send(frame) # Note: this seems to lag based on set fps
+        t7 = time.time()
+        print("T6: ", t7-t6)
+        # virtual_cam.sleep_until_next_frame()
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # cv2.imshow("Left image before rectification", imgL)
+        # cv2.imshow("Right image before rectification", imgR)
+        #
+        # Left_nice= cv2.remap(imgL,Left_Stereo_Map[0],Left_Stereo_Map[1], cv2.INTER_LANCZOS4, cv2.BORDER_CONSTANT, 0)
+        # Right_nice= cv2.remap(imgR,Right_Stereo_Map[0],Right_Stereo_Map[1], cv2.INTER_LANCZOS4, cv2.BORDER_CONSTANT, 0)
+        #
+        # cv2.imshow("Left image after rectification", Left_nice)
+        # cv2.imshow("Right image after rectification", Right_nice)
+        # cv2.waitKey(0)
+
+
+
+        # # Display the resulting frame
+        # cv2.namedWindow("over-under frame", cv2.WINDOW_NORMAL)
+        # cv2.imshow("over-under frame", stacked_frame)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
 
 # When everything done, release the capture
 cap.release()
